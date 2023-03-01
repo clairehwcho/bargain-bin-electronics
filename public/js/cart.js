ef1985190560b0d10e898426d89d3803c6d41770
Patrick <patrick@Patricks-iMac.local>
2023-02-28 18:09:19 -0800

    implementing add to cart

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,5 +1,16 @@
 const addToCart = async (event) => {
+  const response = await fetch('/cart', {
+    method: 'POST',
+    body: JSON.stringify({product_id}),
+    headers: { 'Content-Type': 'application/json' },
+  });
 
+  if (response.ok) {
+    console.log("item added to cart")
+  }
+  } else {
+    alert(response.statusText);
+  }
 
 }
 
