6c6112780ff3fb5d596d94ad4e95ea01e58aac8c
Patrick <patrick@Patricks-iMac.local>
2023-03-01 21:08:44 -0800

    fixing directory problems

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,9 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
-const cartRoutes = require('./cartRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-router.use('/cart', cartRoutes);
+
 
 module.exports = router;
